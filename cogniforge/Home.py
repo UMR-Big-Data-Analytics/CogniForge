314fc37898acb09260889b4b5b5c633737c26595
avinjoseph <avin.chelseafc@gmail.com>
2024-12-20 23:20:58 +0100

    changed the icons

--- cogniforge/Home.py
+++ cogniforge/Home.py
@@ -6,27 +6,27 @@ home = st.Page(
 )
 
 timeseries = st.Page(
-    "pages/1_data_preprocessing/Time_Series_Data.py", title="Time Series", icon=""
+    "pages/1_data_preprocessing/Time_Series_Data.py", title="Time Series", icon="⏳"
 )
 
 image = st.Page(
-    "pages/1_data_preprocessing/Image_Data.py", title="Image Data", icon=""
+    "pages/1_data_preprocessing/Image_Data.py", title="Image Data", icon="📩"
 )
 
 layer = st.Page(
-    "pages/4_layer_quality/Layer_Thickness.py", title="Layer Thickness", icon=""
+    "pages/4_layer_quality/Layer_Thickness.py", title="Layer Thickness", icon="📚"
 )
 
 roughness = st.Page(
-    "pages/3_steel_quality/Roughness.py", title="Roughness", icon=""
+    "pages/3_steel_quality/Roughness.py", title="Roughness", icon="🦿"
 )
 
 rust = st.Page(
-    "pages/3_steel_quality/Rust.py", title="Rust", icon=""
+    "pages/3_steel_quality/Rust.py", title="Rust", icon="💨"
 )
 
 wire = st.Page(
-    "pages/2_wire_quality/Wire_Quality.py", title="Wire Quality", icon=""
+    "pages/2_wire_quality/Wire_Quality.py", title="Wire Quality", icon="🔌"
 )
 
 
@@ -34,10 +34,10 @@ wire = st.Page(
 pg = st.navigation(
     {
         "" : [home],
-        "⏩ Data Preprocessing" : [timeseries,image],
-        "🔌 Wire Quality" : [wire],
-        "🗻 Steel Quality" : [roughness,rust],
-        "💦 Layer Quality" : [layer]
+        "Data Preprocessing" : [timeseries,image],
+        "Wire Quality" : [wire],
+        "Steel Quality" : [roughness,rust],
+        "Layer Quality" : [layer]
     }
 )
 
