66c8161004da7a2e1b0d3d86678bcd13eeaed4d7
avinjoseph <avin.chelseafc@gmail.com>
2025-01-07 00:13:00 +0100

    Removed the pages folder and numbers from folder name.

--- cogniforge/Home.py
+++ cogniforge/Home.py
@@ -2,31 +2,31 @@ import streamlit as st
 
 
 home = st.Page(
-    "pages/main.py", title="Home", icon="🏠", default=True
+    "main.py", title="Home", icon="🏠", default=True
 )
 
 timeseries = st.Page(
-    "pages/1_data_preprocessing/Time_Series_Data.py", title="Time Series", icon="⏳"
+    "data_preprocessing/Time_Series_Data.py", title="Time Series", icon="⏳"
 )
 
 image = st.Page(
-    "pages/1_data_preprocessing/Image_Data.py", title="Image Data", icon="📩"
+    "data_preprocessing/Image_Data.py", title="Image Data", icon="📩"
 )
 
 layer = st.Page(
-    "pages/4_layer_quality/Layer_Thickness.py", title="Layer Thickness", icon="📚"
+    "layer_quality/Layer_Thickness.py", title="Layer Thickness", icon="📚"
 )
 
 roughness = st.Page(
-    "pages/3_steel_quality/Roughness.py", title="Roughness", icon="🦿"
+    "steel_quality/Roughness.py", title="Roughness", icon="🦿"
 )
 
 rust = st.Page(
-    "pages/3_steel_quality/Rust.py", title="Rust", icon="💨"
+    "steel_quality/Rust.py", title="Rust", icon="💨"
 )
 
 wire = st.Page(
-    "pages/2_wire_quality/Wire_Quality.py", title="Wire Quality", icon="🔌"
+    "wire_quality/Wire_Quality.py", title="Wire Quality", icon="🔌"
 )
 
 
