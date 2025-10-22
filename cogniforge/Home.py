23441ab7e2bb6b8f4a611c485865c22bde2ede89
1lutz <lutzgog@gmail.com>
2025-10-22 16:17:02 +0200

    fix path casing to make it work on Linux

--- cogniforge/Home.py
+++ cogniforge/Home.py
@@ -1,25 +1,29 @@
 import streamlit as st
 
-# Define all pages
-home = st.Page("main.py", title="Home", icon="🏠", default=True)
-timeseries = st.Page("data_preprocessing/time_series_data.py", title="Time Series", icon="⏳")
-# image = st.Page("data_preprocessing/image_data.py", title="Image Data", icon="📩")
-layer = st.Page("layer_quality/layer_thickness.py", title="Layer Thickness", icon="💦")
-anomaly = st.Page("fft_analysis/anomaly_detection.py", title="Anomaly Detection", icon="🚨")
-roughness = st.Page("steel_quality/roughness.py", title="Roughness Estimation", icon="🗻")
-rust = st.Page("steel_quality/rust.py", title="Rust Detection", icon="🧱")
-wire = st.Page("wire_quality/wire_quality.py", title="Wire Quality", icon="🔌")
-photo = st.Page("Photo.py", title="Photo Viewer", icon="🖼️")
-fft = st.Page("fft_analysis/fft_analysis.py",title="FFT Analysis", icon="📈")
-
 pg = st.navigation({
-    "": [home],
-    "Data Preprocessing": [timeseries],
-    "Wire Quality": [wire],
-    "Steel Surface": [rust, roughness],
-    "Layer Quality": [layer],
-    "Outlier Detection": [fft, anomaly],
-    "Misc": [photo]
+    "": [
+        st.Page("main.py", title="Home", icon="🏠", default=True)
+    ],
+    "Data Preprocessing": [
+        st.Page("Data_Preprocessing/Time_Series_Data.py", title="Time Series", icon="⏳")
+    ],
+    "Wire Quality": [
+        st.Page("Wire_Quality/Wire_Quality.py", title="Wire Quality", icon="🔌")
+    ],
+    "Steel Surface": [
+        st.Page("steel_quality/Rust.py", title="Rust Detection", icon="🧱"),
+        st.Page("steel_quality/Roughness.py", title="Roughness Estimation", icon="🗻")
+    ],
+    "Layer Quality": [
+        st.Page("layer_quality/Layer_Thickness.py", title="Layer Thickness", icon="💦")
+    ],
+    "Outlier Detection": [
+        st.Page("FFT_Analysis/FFT_Analysis.py",title="FFT Analysis", icon="📈"),
+        st.Page("FFT_Analysis/anomaly_detection.py", title="Anomaly Detection", icon="🚨")
+    ],
+    "Misc": [
+        st.Page("Photo.py", title="Photo Viewer", icon="🖼️")
+    ]
 })
 
 pg.run()
